b01fcb9374278f748be84f3504ca75913595f29e
KristiBelic <kristinovoselic@gmail.com>
2023-06-26 12:59:57 +0200

    ispravakZ02

--- Edunova04/src/edunova/Z02.java
+++ Edunova04/src/edunova/Z02.java
@@ -7,21 +7,17 @@ public class Z02 {
 		// Program ispisuje brojeve od 100 do 1
 		// u istom redu odvojeno zarezom
 
-	//	for (int i = 100; i >= 0; i--) {
-		//	System.out.print(i + (i != 1 ? "," : ""));
+		for (int i = 100; i >= 1; i--) {
+			System.out.print(i + (i != 1 ? "," : ""));
 
-			
-			
-	
-			
 			for (int i = 100; i > 0; i--) {
-				if (i !=1 ) {
-					System.out.print(i + ",");
+				if (i != 1) {
+					// System.out.print(i + ",");
 				} else {
 					System.out.print(i);
 				}
 			}
 		}
 
-	
+	}
 }
