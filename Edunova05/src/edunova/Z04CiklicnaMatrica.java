bd594adf3916e96db4b91b4702fe804a4a9d3737
KristiBelic <kristinovoselic@gmail.com>
2023-07-05 11:59:37 +0200

    Zavrsena matrica

--- Edunova05/src/edunova/Z04CiklicnaMatrica.java
+++ Edunova05/src/edunova/Z04CiklicnaMatrica.java
@@ -2,76 +2,76 @@ package edunova;
 
 import javax.swing.JOptionPane;
 
-public class Ciklicnamatrica {
+public class CiklicnaMatrica04 {
 
 	public static void main(String[] args) {
-	
-		boolean dev=true;
-		
-		int red=dev ? 5 : Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redova"));
-		int stupac=dev ? 5 : Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca"));
 		
 		
-		int [] [] matrica = new int [red][stupac];
+		boolean dev=false;
 		
-		
+		int red =dev ? 5 : Integer.parseInt(JOptionPane.showInputDialog("Unesite broj redova"));
+		int stupac = dev ? 5 :Integer.parseInt(JOptionPane.showInputDialog("Unesite broj stupaca"));
+
+		int matrica[][] = new int[red][stupac];
+
 		int b = 1;
-		for (int i = 1; i <= stupac; i++) {
-			matrica[red - 1][stupac - i] = b++;
-			
+
+		int prviRed = red - 1;
+		int zadnjiRed = 0;
+
+		int prviStupac = stupac - 1;
+		int zadnjiStupac = 0;
+
+		while (b <= red * stupac) {
+
+			for (int i = prviStupac; i >= zadnjiStupac; i--) {
+
+				if (b > red * stupac) {
+
+					break;
+
+				} else {
+					matrica[prviRed][i] = b++;
+				}
+
+			}
+			prviRed--;
+			for (int i = prviRed; i >= zadnjiRed; i--) {
+				if (b > red * stupac) {
+					break;
+
+				} else {
+					matrica[i][zadnjiStupac] = b++;
+				}
+
+			}
+			zadnjiStupac++;
+
+			for (int i = zadnjiStupac; i <= prviStupac; i++) {
+				if (b > red * stupac) {
+					break;
+
+				} else {
+					matrica[zadnjiRed][i] = b++;
+				}
+			}
+			zadnjiRed++;
+
+			for (int i = zadnjiRed; i <= prviRed; i++) {
+				if (b > red * stupac) {
+
+				} else {
+					matrica[i][prviStupac] = b++;
+				}
+			}
+			prviStupac--;
+
+			for (int i = 0; i < red; i++) {
+				for (int j = 0; j < stupac; j++) {
+					System.out.print(matrica[i][j] + "\t");
+				}
+				System.out.println();
+			}
 		}
-
-		
-		
-		for(int i=2;i<=red;i++) {
-			matrica[red-i][0]=b++;
-		}
-
-		
-		
-		for(int i=1;i<stupac;i++) {
-			matrica[0][i]=b++;
-		}
-
-	
-	for(int i =1;i<red-1;i++) {
-		matrica[i][stupac-1]=b++;
 	}
-	
-	
-
-	
-	for(int i=stupac-2;i>0;i--) {
-		matrica[red-2][i]=b++;
-	}
-
-	
-	for(int i =red-3;i>0;i--) {
-		matrica[i][1]=b++;
-	}
-	
-
-	
-	for(int i=2;i<stupac-1;i++) {
-		matrica[1][i]=b++;
-	}
-
-	
-	for(int i=2;i<red-3;i++) {
-		matrica[i][stupac-2]=b++;
-	}
-
-	
-	for(int i=stupac-2;i>1;i--) {
-		matrica[red-3][i]=b++;
-	}
-	for (int i = 0; i < red; i++) {
-		for (int j = 0; j < stupac; j++) {
-			System.out.print(matrica[i][j] + "\t");
-		}
-		System.out.println();
-	}
-	System.out.println("**********************************");
-	}
-
 }
